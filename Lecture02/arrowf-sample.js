b6646bc0bf5c652df2e7d0a85395078bf58247b2
savevenjer <savevenjer@gmail.com>
2024-12-04 11:01:04 +0700

    .

---
+++ Lecture02/arrowf-sample.js
@@ -0,0 +1,17 @@
+const sum =(firstParam, secondparam) => {
+    return firstParam + secondparam;
+};
+console.log(sum(2,5));
+
+const printHello = () => {
+    console.log("Hello");
+}
+printHello();
+
+const checkWeight = (weight) =>{
+    console.log(`Baggage weight : ${weight} kilograms`)
+};
+checkWeight(25);
+
+const multyply = (a,b) => a*b;
+console.log(multyply(2,30));
---
+++ Lecture02/strsplt.js
@@ -0,0 +1,9 @@
+const str = 'The quick brow fox jumps over the lazy dog.';
+
+const words = str.split(' ');
+console.log(words[3]);
+// expected output : "fox"
+
+const chars = str.split('');
+console.log(chars[8]);
+// expected output : "k"
