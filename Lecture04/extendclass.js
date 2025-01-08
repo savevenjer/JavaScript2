dee9089b7bd882ecdf06629a994cfdef2690cd33
savevenjer <savevenjer@gmail.com>
2025-01-08 08:58:34 +0700

    1

---
+++ Lecture04/extendclass.js
@@ -0,0 +1,23 @@
+// Parent class
+class Media{
+    constructor(info) {
+        this.publishDate = info.publishDate;
+        this.name = info.name;
+    }
+}
+
+// child calss
+class Song extends Media{
+    constructor(songData){
+        super(songData);
+        this.artist = songData.artist;
+    }
+}
+
+const mySong = new Song({
+    artist: "Queen",
+    name: "Bohemian Rhapsody",
+    publishDate: 1975,
+});
+
+console.log(mySong);
---
+++ Lecture04/libHello.js
@@ -0,0 +1,19 @@
+// libHello.js
+const person ={
+    name: 'Supawich',
+    weight: 85
+}
+
+function sayHello(){
+    console.log("Hello World!")
+}
+
+function cube(x){
+    return x*x*x;
+}
+
+const add =(a,b) => a+b;
+
+const status = true;
+
+module.exports = {person, sayHello,cube, add, status}
